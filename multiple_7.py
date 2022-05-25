0a0f3a35597b0729d892505412db9a50ede297f4
Aleksandr <aleksandr955@gmail.com>
2022-05-25 12:34:59 +0300

    Put operator 'not'

--- multiple_7.py
+++ multiple_7.py
@@ -1,7 +1,7 @@
 # Выведите на экран все числа в диапазоне от 1 до 100 кратные 7
 
 for i in range(1, 100):
-    if i % 7 == 0:
+    if not i % 7:  # put 'not' instead of '== 0'
         print(i, end=' ')
 
 
