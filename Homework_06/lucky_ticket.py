a87090fbf852463b67cfd36ab90c285dd63996d7
Aleksandr <aleksandr955@gmail.com>
2022-05-31 12:25:53 +0300

    add solution for bigger amount of numbers

--- Homework_06/lucky_ticket.py
+++ Homework_06/lucky_ticket.py
@@ -5,26 +5,34 @@
 # равна 13, а правая 22, и оно является счастливым билетом (т. к. 1 + 3 = 2 + 2).
 # 2 variants of solution
 
-n = int(input('Enter ticket number: '))
-s = list(map(int, str(n)))  # use function map() to change type of variables
-if sum(s[:2]) == sum(s[2:]):
+# n = int(input('Enter ticket number: '))
+# s = list(map(int, str(n)))  # use function map() to change type of variables
+# if sum(s[:2]) == sum(s[2:]):
+#     print('Lucky')
+# else:
+#     print('Ordinary')
+#
+#
+# number = int(input('Enter ticket number: '))
+# list_str_num = list(str(number))  # made list with str elements from int
+# sp = []
+# for i in list_str_num:
+#     sp.append(int(i))  # made int in list to sum integers in the list
+# print(sp)  # check that list consist from int numbers
+# if sum(sp[:2]) == sum(sp[2:]):
+#     print('Lucky ticket')
+# else:
+#     print('Ordinary ticket')
+#
+
+
+n = int(input('Enter number =>'))
+s = str(n)
+sp = [int(n) for n in s]
+if not sum(sp) % 2:
     print('Lucky')
 else:
     print('Ordinary')
-
-
-number = int(input('Enter ticket number: '))
-list_str_num = list(str(number))  # made list with str elements from int
-sp = []
-for i in list_str_num:
-    sp.append(int(i))  # made int in list to sum integers in the list
-print(sp)  # check that list consist from int numbers
-if sum(sp[:2]) == sum(sp[2:]):
-    print('Lucky ticket')
-else:
-    print('Ordinary ticket')
-
-
 
 
 
