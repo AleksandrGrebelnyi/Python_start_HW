d5ae7a2ba3366fd625496857113ba9226e3da48f
Aleksandr <aleksandr955@gmail.com>
2022-06-02 16:03:33 +0300

    add shorter solution

--- Homework_07/pi_digits_after_dot.py
+++ Homework_07/pi_digits_after_dot.py
@@ -2,5 +2,14 @@
 # должно быть 2 знака после запятой, во второй 3 и так далее
 
 from math import pi
-print(pi)
-print(f'{pi:.2f}\n{pi:.3f}\n{pi:.4f}\n{pi:.5f}\n{pi:.6f}\n{pi:.7f}\n{pi:.8f}\n{pi:.9f}\n{pi:.10f}')
+for i in range(2, 12):
+    print(round(pi, i))
+# print(pi)
+# print(f'{pi:.2f}\n{pi:.3f}\n{pi:.4f}\n{pi:.5f}\n{pi:.6f}\n{pi:.7f}\n{pi:.8f}\n{pi:.9f}\n{pi:.10f}')
+
+
+
+
+
+
+
