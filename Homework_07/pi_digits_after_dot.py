441cb0b9bd72390644d647ca22edc0a5968ffab1
Aleksandr <aleksandr955@gmail.com>
2022-06-02 16:44:26 +0300

    add solution with f'

--- Homework_07/pi_digits_after_dot.py
+++ Homework_07/pi_digits_after_dot.py
@@ -3,10 +3,15 @@
 
 from math import pi
 for i in range(2, 12):
-    print(round(pi, i))
+    print(f'{pi:.{i}f}')
+
+
+# for i in range(2, 12):
+#     print(round(pi, i))
+
+
 # print(pi)
 # print(f'{pi:.2f}\n{pi:.3f}\n{pi:.4f}\n{pi:.5f}\n{pi:.6f}\n{pi:.7f}\n{pi:.8f}\n{pi:.9f}\n{pi:.10f}')
-
 
 
 
