a8d745c51449c4b4cfb270e5ff72543c6c51e9cb
Aleksandr <aleksandr955@gmail.com>
2022-06-02 11:09:36 +0300

    add shorter solution

--- Homework_07/longest_word.py
+++ Homework_07/longest_word.py
@@ -1,8 +1,13 @@
 # Вводится строка из слов, разделенных пробелами. Найти самое длинное
 # слово и вывести его на экран. Added displayed list of lengths and number the longest word
 
-
 t = input('Enter text with spaces: ').split()
-res = list(map(lambda x: len(x), t))
-print(f'Longest word is => {max(t, key=len)}\nAmount of numbers => {res}\nLongest word => {max(res)}')
+print(f'Longest word is => {max(t, key=len)}')
+
+
+# t = input('Enter text with spaces: ').split()
+# res = list(map(lambda x: len(x), t))
+# print(f'Longest word is => {max(t, key=len)}\nAmount of numbers => {res}\nLongest word => {max(res)}')
+
+
 
