3bf236b4806ee32b5fbf870c3f77c36b944325cc
Aleksandr <aleksandr955@gmail.com>
2022-06-07 21:24:05 +0300

    add new solution

--- Homework_07/clear_from_html_tags.py
+++ Homework_07/clear_from_html_tags.py
@@ -6,15 +6,26 @@
 # - тэг внутри себя, может содержать кучу доп. информации.
 # Например <div id="rcnt" style="clear:both;position:relative;zoom:1">
 
+html_text = '<p>5 < 4</p> Hi<div id="rcnt" style="clear:both;position:relative;zoom:1">'
+tag_del = ['p', 'a', 'h1', 'div']
+for tag in tag_del:
+    while f'<{tag}' in html_text:
+        start = html_text.find(f'<{tag}')
+        stop = html_text.find(f'>', start + 1)
+        html_text = html_text.replace(html_text[start: stop + 1], '')
+        html_text = html_text.replace(f'</{tag}>', '')
+
+print(html_text)
+
+
 import bleach
 
-tag_text = '<p>Hello, <b>World</b></p> Hi<div id="rcnt" style="clear:both;position:relative;zoom:1">'
+tag_text = '<p>5 < 4</p> Hi<div id="rcnt" style="clear:both;position:relative;zoom:1">'
 print(bleach.clean(tag_text, tags=[], strip=True))
 
 
-
-tag_text = '<p>Hello, <b>World</b></p> Hi<div id="rcnt" style="clear:both;position:relative;zoom:1">'
-
+# tag_text = '<p>Hello, <b>World</b></p> Hi<div id="rcnt" style="clear:both;position:relative;zoom:1">'
+tag_text = '<p>5 < 4</p> Hi<div id="rcnt" style="clear:both;position:relative;zoom:1">'
 while '>' in tag_text:
     x1, x2 = tag_text.find('<'), tag_text.find('>')
     tag_text = tag_text.replace(tag_text[x1:x2 + 1], '')
